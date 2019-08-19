10544f9df07013811b79568ce2bcf7362129f72e
Johannes Winkelmann <jwi@sensirion.com>
2019-08-19 15:28:35 +0200

    add brief comment describing the demo

--- examples/ess-human-presence-detector/ess-human-presence-detector.ino
+++ examples/ess-human-presence-detector/ess-human-presence-detector.ino
@@ -6,6 +6,20 @@
 
 SensirionESS ess;
 
+// this example is using the SGP30's eCO2 signal to detect human presence
+//
+// During warmup, the yellow LED is on to show that the demo is not ready yet
+// After warmup, the yellow LED will turn off, and the red LED will turn on/off
+// depending on whether the measured eCO2 signal is above the threshold  or not
+//
+// Note that this is meant for demonstrator purposes only, as the threshold
+// for the "presence" signal is that the signal is 20% (1.2x) higher than
+// the initial reading (initial eCO2 signal after warmup)
+//
+// Limitations: the threshold will not adjust until the board is rebooted.
+// As such, if the eCO2 in the room changes, the lowest values may go
+// beyond the threshold already. In that case, simply reset the board
+
 #define SGP_WARMUP_COUNT 20
 
 void setup()
